847bd5e66cac14bfcd2ba4e80644186c29cdfc3a
Sergio Muro <sergio.muroc@pucp.pe>
2020-06-15 20:43:33 -0500

    subir commit

--- src/main/java/sw2/lab6/teletok/controller/PostController.java
+++ src/main/java/sw2/lab6/teletok/controller/PostController.java
@@ -1,10 +1,27 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.repository.PostCommentRepository;
+import sw2.lab6.teletok.repository.PostRepository;
+
+import java.util.List;
+
 
 @Controller
 public class PostController {
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    PostCommentRepository postCommentRepositoryRepository;
+
 
     @GetMapping(value = {"", "/"})
     public String listPost(){
@@ -23,21 +40,33 @@ public class PostController {
 
     @GetMapping("/post/file/{media_url}")
     public String getFile() {
+
+
+
         return "";
     }
 
     @GetMapping("/post/{id}")
-    public String viewPost() {
+    public String viewPost(Model model,@RequestParam("id") int id) {
+
+        model.addAttribute("listaPost",postRepository.findById(int id));
+        model.addAttribute("listaPostComment",postRepository.findById(int id));
+
         return "post/view";
     }
 
     @PostMapping("/post/comment")
     public String postComment() {
+
+
+
         return "";
     }
 
     @PostMapping("/post/like")
     public String postLike() {
+
+
         return "";
     }
 }
